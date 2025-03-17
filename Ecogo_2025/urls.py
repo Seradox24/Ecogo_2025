7231b7302e94e113caa933a097abdb6b1b28065e
Seradox24 <ma.inayao@alumnos.duoc.cl>
2025-03-17 05:55:31 -0300

    Guardando cambios locales antes de actualizar

--- Ecogo_2025/urls.py
+++ Ecogo_2025/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     #path('core/', include('core.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
